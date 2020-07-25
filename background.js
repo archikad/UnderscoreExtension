974a3f62994c1dd8143f6759c7eed77aa82a5e1a
Michael <michaelwilliamtang@gmail.com>
2020-07-25 13:52:10 -0700

    More cleanup

---
+++ background.js
@@ -0,0 +1,15 @@
+// const saveExtractedText = (data) => {
+//     $.post('http://localhost:3000/receiver', data);
+// }
+
+// chrome.runtime.onMessage.addListener(
+//     (request, sender, senderResponse) => {
+//         switch (request.message) {
+//             case 'save_text': {
+//                 saveExtractedText(request.data);
+//                 break;
+//             }
+//             default:
+//         }
+//     }
+// );
--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let postButton = document.getElementById('postButton');
-    postButton.addEventListener('click', function () {
+    let postButton = document.getElementById('post-button');
+    postButton.addEventListener('click', () => {
         chrome.tabs.getSelected(null, (tab) => {
-            let fm = document.createElement('form');
-            fm.action = 'http://gtmetrix.com/analyze.html?bm';
-            fm.method = 'post';
-            let i = document.createElement('input');
-            i.type = 'hidden';
-            i.name = 'url';
-            i.value = tab.url;
-            fm.appendChild(i);
-            document.body.appendChild(f);
-            fm.submit();
+            // let fm = document.createElement('form');
+            // fm.action = 'http://gtmetrix.com/analyze.html?bm';
+            // fm.method = 'post';
+            // let i = document.createElement('input');
+            // i.type = 'hidden';
+            // i.name = 'url';
+            // i.value = tab.url;
+            // fm.appendChild(i);
+            // document.body.appendChild(fm);
+            // fm.submit();
         });
     }, false);
 }, false);
